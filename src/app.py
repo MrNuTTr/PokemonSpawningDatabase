483bb5e3d08a0dd17dc9fae4752a8274d5c4b475
Eli <43860787+MrNuTTr@users.noreply.github.com>
2023-04-30 17:47:34 -0400

    Done, I guess.

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import psycopg2
 import random
 from flask import Flask, render_template, request, url_for, redirect
@@ -114,7 +116,7 @@ def client(client_id):
     return render_template('client.html', client=client_info, pokemon=poke)
 
 
-@app.route('/bookings/')
+@app.route('/booking/')
 def bookings():
     conn = get_db_connection()
     cur = conn.cursor()
@@ -157,15 +159,21 @@ def add_pokemon():
         dex = request.form['pokedex']
         name = request.form['name']
         level = request.form['level']
-        gender = request.form.get('gender')
+        gender = request.form['gender']
+        dob = request.form['dob']
+        is_child = bool(request.form['is_child'])
 
         cur.execute('INSERT INTO Pokemon '
-                    'VALUES (%s, %s, %s, %s, %s, %s)',
-                    (poke_id, owner, dex, name, level, gender))
+                    'VALUES (%s, %s, %s, %s, %s, %s, %s)',
+                    (poke_id, owner, dex, name, level, gender, dob))
         conn.commit()
         cur.close()
         conn.close()
-        return redirect(url_for('pokemon', pokemon_id=poke_id))
+
+        if is_child:
+            return redirect(url_for('add_child', client_id=owner, pokemon_id=poke_id))
+        else:
+            return redirect(url_for('pokemon', pokemon_id=poke_id))
 
     cur.execute('SELECT * FROM Client;')
     owners = cur.fetchall()
@@ -176,6 +184,116 @@ def add_pokemon():
     cur.close()
     conn.close()
     return render_template('add_pokemon.html', owners=owners, pokedex=dex)
+
+
+@app.route('/client/<client_id>/add/child/<pokemon_id>/', methods=('GET', 'POST'))
+def add_child(client_id, pokemon_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    if request.method == 'POST':
+        father = request.form['father']
+        mother = request.form['mother']
+
+        cur.execute('INSERT INTO Family VALUES (%s, %s, %s)',
+                    (pokemon_id, father, mother))
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return redirect(url_for('pokemon', pokemon_id=pokemon_id))
+
+    cur.execute(f"SELECT * FROM Pokemon WHERE owner_id={client_id} and poke_id<>{pokemon_id} and gender='M'")
+    males = cur.fetchall()
+
+    cur.execute(f"SELECT * FROM Pokemon WHERE owner_id={client_id} and poke_id<>{pokemon_id} and gender='F'")
+    females = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return render_template('add_child.html', males=males, females=females)
+
+
+@app.route('/booking/add/', methods=('GET', 'POST'))
+def select_client():
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    if request.method == 'POST':
+        client_id = request.form['owner_id']
+        return redirect(url_for('add_booking', client_id=client_id))
+
+    cur.execute('SELECT * FROM Client;')
+    owners = cur.fetchall()
+
+    return render_template('select_client.html', owners=owners)
+
+
+@app.route('/booking/add/<client_id>/', methods=('GET', 'POST'))
+def add_booking(client_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    if request.method == 'POST':
+        booking_id = random.randint(a=10000, b=99999)
+        room_num = request.form['room_num']
+        poke_id = request.form['pokemon']
+        date_in = request.form['date_in']
+        date_out = request.form['date_out'] or None
+
+        cur.execute('INSERT INTO Booking '
+                    'VALUES (%s, %s, %s, %s, %s);',
+                    (booking_id, room_num, poke_id, date_in, date_out))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('bookings'))
+
+    cur.execute(f'SELECT room_num FROM storage '
+                f'EXCEPT '
+                f'SELECT room_num '
+                f'FROM Storage NATURAL JOIN Booking '
+                f'WHERE date_out is null')
+    open_rooms = cur.fetchall()
+
+    cur.execute(f'SELECT poke_id, owner_id, pokedex_num, name, level, gender, dob '
+                f'FROM Pokemon WHERE owner_id = {client_id} '
+                f'EXCEPT '
+                f'SELECT poke_id, owner_id, pokedex_num, name, level, gender, dob '
+                f'FROM Pokemon NATURAL JOIN Booking WHERE date_out is null')
+    poke = cur.fetchall()
+
+    cur.close()
+    conn.close()
+    return render_template('add_booking.html', rooms=open_rooms, pokemon=poke)
+
+
+@app.route('/booking/update/<booking_id>/', methods=['GET', 'POST'])
+def update_booking(booking_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute(f'SELECT * FROM Booking WHERE booking_id={booking_id}')
+    booking = cur.fetchone()
+
+    if booking[4] is not None:
+        return render_template('error.html', errortext="Cannot update an entry for past resident.")
+
+    if request.method == "POST":
+        date_out = request.form['date_out']
+        cur.execute('UPDATE Booking '
+                    'SET date_out=%s '
+                    'WHERE booking_id=%s',
+                    (date_out, booking_id))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('bookings'))
+
+    cur.close()
+    conn.close()
+    return render_template('update_booking.html')
 
 
 app.run(debug=True)
